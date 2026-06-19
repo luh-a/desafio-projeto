07c41c535ebc47e2da669760f68ab388789f2569
Anna Luz <Noite.Cetafaju@labfies.org.br>
2026-06-19 19:02:16 -0300

    Criação do script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-const formDesconto = document.querySelector("#formulario-desconto")
-const divResultado = document.querySelector("#div-lista-pessoas")
+//criar um array de objetos pessoa
+const pessoas = []
 
-formDesconto
+//pegando elementos do DOM
+const formPessoa = document.querySelector("#formulario-pessoa")
+const divPessoa = document.querySelector("#div-lista-pessoas")
+
+//capturando o evento do submit do formulário
+formPessoa.addEventListener('submit', (evt)=>{
+    //interrompendo o efeito padrão do formulário
+    evt.preventDefault()
+
+    //criando o objeo data form
+    const dadosForm = new FormData(formPessoa)
+
+    //criando e preenchendo o objeto literal pessoa
+    const pessoa = {
+        nome: dadosForm.get('nome'),
+        idade: dadosForm.get('idade'),
+        renda: dadosForm.get('renda')
+    }
+
+    //chamando a função addPessoa e passando o objeto literal pessoa
+    addPessoa(pessoa)
+
+    //limpando o formulário
+    formPessoa.requestFullscreen()
+})
